1863a3627f348e1974c57b62917d43239f0cc49f
Mazaher Muraj <mmuraj.313@gmail.com>
2019-09-01 19:05:09 +0100

    fetches request

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('connected!')
+let endpoint = 'http://api.coinlayer.com/api/live?access_key=1b560d83dbab812e2781a03d1d38eb32&target=GBP&symbols=BTC,ETH,BCH,LTC,DASH,XRP,XLM'
+
+fetch(endpoint)
+  .then(res => {
+    if (!res.ok) {
+      return Promise.reject('Something went wrong')
+    }
+    return res.json()
+  })
+  .then(data => {
+    let rates = data.rates
+    let arrOfRates = Object.keys(rates).map(key => {
+      return [key, rates[key]]
+    })
+    console.log(arrOfRates)
+    console.log('THE RATES', data.rates)
+    const root = document.getElementById('root')
+    root.innerHTML = arrOfRates
+  })
+  .catch(error => {
+    console.log('error is', error)
+  })
