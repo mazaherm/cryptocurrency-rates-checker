ac962e93499e8f4c0819ea855f55ca54724abce5
Mazaher Muraj <mmuraj.313@gmail.com>
2019-09-07 17:02:25 +0100

    creates dynamic list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,11 +12,24 @@ fetch(endpoint)
     let arrOfRates = Object.keys(rates).map(key => {
       return [key, rates[key]]
     })
-    console.log(arrOfRates)
-    console.log('THE RATES', data.rates)
     const root = document.getElementById('root')
     root.innerHTML = arrOfRates
   })
   .catch(error => {
     console.log('error is', error)
   })
+
+/**
+ * Creates a dynamic list which takes in an array
+ */
+createList = array => {
+  let list = document.createElement('ul')
+
+  for (let i = 0; i < array.length; i++) {
+    let listItem = document.createElement('li')
+
+    listItem.appendChild(document.createTextNode(array[i]))
+    list.appendChild(listItem)
+  }
+  return list
+}
